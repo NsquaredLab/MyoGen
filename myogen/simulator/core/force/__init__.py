0a3c0ec1ea680f839f8597fbb5a8fb316ba738d9
Raul C. Sîmpetru <raul.simpetru@fau.de>
2025-08-31 13:23:09 +0200

    feat: remove unused muscle model imports from __init__.py

--- myogen/simulator/core/force/__init__.py
+++ myogen/simulator/core/force/__init__.py
@@ -1,13 +1,8 @@
 from .force_model import ForceModel
-from .hill_muscle_model import HillMuscleModel, HillMuscleState
-from .force_integration import IntegratedForceModel
 from .biomechanics import JointBiomechanics, MuscleGeometry, JointGeometry
 
 __all__ = [
     "ForceModel",
-    "HillMuscleModel",
-    "HillMuscleState",
-    "IntegratedForceModel",
     "JointBiomechanics",
     "MuscleGeometry",
     "JointGeometry",
